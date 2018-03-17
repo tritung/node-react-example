e30147675996a13d6f34127cd75aafe3bb3b6c5d
tungtruong-lunex <tungtruong@lunextelecom.com>
2018-03-17 16:55:05 +0700

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+
+      default:
+        return <a href="/api/logout">Logout</a>;
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Node-React</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
