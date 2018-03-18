6ecf3c78eaefeb753b1b8d81cbe7da4fdd055aee
tungtruong-lunex <tungtruong@lunextelecom.com>
2018-03-18 14:42:00 +0700

    update

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Emaily!</h1>
+      Collet feedback form your user
+    </div>
+  );
+};
+
+export default Landing;
