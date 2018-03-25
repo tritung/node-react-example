fdba1f9092ebb83e8fce87cc6cc4479240b8f9ae
tungtruong-lunex <tungtruong@lunextelecom.com>
2018-03-25 14:46:26 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 const keys = require("./config/keys");
@@ -26,6 +27,7 @@ app.use(passport.session());
 require("./routes/homeRoutes")(app);
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
