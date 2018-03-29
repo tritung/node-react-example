4b98de113b1a3588f11112cd6d3b7e71ffb017ac
tungtruong-lunex <tungtruong@lunextelecom.com>
2018-03-29 22:33:10 +0700

    update

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 //Survey New shows SurveyForm and SurveyFromReview
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 import SurveyForm from "./SurveyForm";
 import SurveyFromReview from "./SurveyFormReview";
 
@@ -26,4 +27,6 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyNew);
